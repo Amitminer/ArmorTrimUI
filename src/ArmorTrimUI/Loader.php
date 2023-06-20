0d49ec63834f59c0a433bfc5d97b26b306a7b155
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-06-20 13:53:38 +0530

    update Loader.php

--- src/ArmorTrimUI/Loader.php
+++ src/ArmorTrimUI/Loader.php
@@ -1,1 +1,27 @@
+<?php
 
+declare(strict_types = 1);
+
+namespace ArmorTrimUI;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\player\Player;
+use pocketmine\command\CommandSender;
+use ArmorTrimUI\Manager\FormManager;
+
+class Loader extends PluginBase
+{
+    public function onLoad(): void
+    {
+        $this->getLogger()->info("§aEnabled ArmorTrimUI!");
+    }
+    public function onCommand(CommandSender $sender,Command $command,string $label,array $args): bool {
+        switch ($command->getName()) {
+            case "trims":
+                $formM = new FormManager();
+                $formM->getTrimsUI($sender);
+                return true;
+        }
+    }
+}
